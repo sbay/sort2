c8005fa78498c617ff5336ec7605e20aa3aa7969
Andrei Vassilenko <avassilenko2@sbcglobal.net>
2013-01-17 10:01:39 -0800

    PDF letter server side

--- PDFLetter.php
+++ PDFLetter.php
@@ -8,25 +8,40 @@
 
 if( $_REQUEST['action'] == 'letters' )
 {
+	$hostName		= $_SERVER[SERVER_NAME]; 
+	$dirBrows		=  "." . DIRECTORY_SEPARATOR . "tmp";	
 	
+	if( $_REQUEST['directory'] == true )
+	{
+		$mask 	= $dirBrows . DIRECTORY_SEPARATOR . "*.*";
+		array_map( "unlink", glob( $mask ) );
+	}
 	
-	$records = null;
-	$records = $_REQUEST['records'];
+	$records 	= null;
+	$records 	= $_REQUEST['records'];
 	
 	require_once 'fpdf.php';
 	
+	
+	
+	
+	if(!file_exists($dirBrows))
+	{
+		mkdir($dirBrows);
+	}
 
 	$pdf = new FPDF();
 	$pdf->SetFont('Times', 'B', 15);
 	$pdf->AddPage();
 
-	$dirBrows		= $_REQUEST['directory'];
-	$fileName 		= $dirBrows .  DIRECTORY_SEPARATOR   . trim($records[0]) . trim($records[1]) . trim($records[2]) . ".pdf" ;
+	
+	$fileName 		= $dirBrows .  DIRECTORY_SEPARATOR   . trim($records[0]) . "_"  . trim($records[1]) . "_" . trim($records[2]) . ".pdf" ;
 	$fullName 		= $records[0] . " " . $records[1] . " " . $records[2] . "\n";
 	$fullAdress		= $records[3] . "\n" . $records[4] . "\n" . $records[5] . "\n" .  $records[6] . "\n";
 
+	$linkFileName	= "http:" . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . $hostName . DIRECTORY_SEPARATOR . $fileName;
 	$pdf->Write(5, $fullName . $fullAdress );
 	$pdf->Output( $fileName ,'F');
-	echo  $fileName;
+	echo  $linkFileName;
 	}
 ?>
